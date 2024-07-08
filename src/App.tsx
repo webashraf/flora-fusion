5179adaf0c98e17a79bdbdb84c43ee2c1072e408
Ali Ashraf <89378903+webashraf@users.noreply.github.com>
2024-07-08 23:23:31 +0600

    Connect my flora fussing trea plant full stack project connect to my git hub

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import { Button } from "@/components/ui/button";
+
+function App() {
+  return (
+    <>
+      {/* <h1 className="text-3xl font-bold underline text-red-700">Hello world!</h1> */}
+      <Button>Welcome to fusion world</Button>
+    </>
+  );
+}
+
+export default App;
